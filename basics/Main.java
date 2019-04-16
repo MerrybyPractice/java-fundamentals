13a0d11534e6fe43d3dacbd05d8b6baec33f36ea
Merry Cimakasky <mkcimakasky@gmail.com>
2019-04-16 09:43:28 -0700

    testing plural

---
+++ basics/Main.java
@@ -0,0 +1,34 @@
+public class Main{
+  public static void main(String[] args){
+    int catCount = 2; 
+    System.out.println("I own" + catCount + " " + pluralize("cat", catCount)+".");
+
+  }
+    
+    public static String pluralize(String plural, int quantAnimal){ 
+      char s; 
+      s = 's'; 
+      
+      if(quantAnimal == 0 || quantAnumal > 1 ){
+        plural = String+s;
+      }else{
+        plural = String; 
+      }
+      
+      return plural;
+    }
+}
+  
+//Pluralize -- needs to return a string
+  //needs to accpect a word and a number
+  //return a string with the word pluralized (ie, an s added on to the end) if hte number is 0 or greater than one
+  //if number is one, do not add an s
+//flipping coins -- returns nothing to main, prints as goes
+  //takes in integer n, flips until n heads are flipped in a row 
+  //random number between o and 1
+    //below .5 tails 
+    //above .5 heads 
+  //print out heads or tails accordingly on one line for each flip 
+  //print "It took x flips to flip n heads in a row." after n has been achieved. 
+//command line clock 
+  //stretch goals: how fast in hertz? 
